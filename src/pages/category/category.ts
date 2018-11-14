2af70555bb638d66998909c0ab58a6e9512b8d50
2Fe <hee861213@naver.com>
2018-11-14 16:23:07 +0900

    category tab 기본 레이아웃 추가

--- src/pages/category/category.ts
+++ src/pages/category/category.ts
@@ -15,6 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoryPage {
 
+  productCategories =  [
+    "정육", "청과", "쌀잡곡", "채소", 
+    "계란", "유제품", "냉장냉동", "라면류", 
+    "통조림류", "김", "해조류", "조미료", 
+    "장류", "생수", "커피/음료", "과자류",
+    "시리얼류", "빵류", "휴지", "주방용품",
+    "구강용품", "바디용품", "세제류", "잡화류"
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
