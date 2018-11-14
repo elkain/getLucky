a172e84710d3320e52456cc424b5312466840ed1
2Fe <hee861213@naver.com>
2018-11-14 19:38:48 +0900

    category 페이지 추가

--- src/pages/category/category.ts
+++ src/pages/category/category.ts
@@ -15,20 +15,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoryPage {
 
-  productCategories =  [
-    "정육", "청과", "쌀잡곡", "채소", 
-    "계란", "유제품", "냉장냉동", "라면류", 
-    "통조림류", "김", "해조류", "조미료", 
-    "장류", "생수", "커피/음료", "과자류",
-    "시리얼류", "빵류", "휴지", "주방용품",
-    "구강용품", "바디용품", "세제류", "잡화류"
-  ];
+  productAllCategories;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoryPage');
+
+    this.productAllCategories = [
+      { category: "정육", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "청과", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "쌀잡곡", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "채소", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "계란", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "유제품", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "냉장냉동", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "라면류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "통조림류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "김", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "해조류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "조미료", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "장류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "생수", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "커피/음료", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "과자류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "시리얼류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "빵류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "휴지", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "주방용품", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "구강용품", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "바디용품", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "세제류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] },
+      { category: "잡화류", subCategories: ["전체", "소고기(한우)", "돼지고기", "닭고기"] }
+    ];
+
+    for (let i = 0; i < this.productAllCategories.length; i++){
+      this.productAllCategories[i].selected = false;
+    }
   }
 
+  categorySelect(category){
+    let idx = this.productAllCategories.indexOf(category);
+
+    if (this.productAllCategories[idx].selected == true){
+      this.productAllCategories[idx].selected = false;
+    }else{
+      for (let i = 0; i < this.productAllCategories.length; i++) {
+        this.productAllCategories[i].selected = false;
+      }
+
+      this.productAllCategories[idx].selected = true;
+    }
+  }
 }
