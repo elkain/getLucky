a058877202b142ad64b3fb458c0ced669591c5b4
2Fe <hee861213@naver.com>
2018-11-16 17:16:51 +0900

    마이럭키 페이지 추가

--- src/pages/mypage/mypage.ts
+++ src/pages/mypage/mypage.ts
@@ -15,11 +15,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MypagePage {
 
+  loginTabs = ["회원", "비회원"];
+  loginTabsSelected;
+
+  username: string;
+  password: string;
+
+  usernamePlaceHolder: string = "아이디";
+  passwordPlaceHolder: string = "비밀번호";
+
+  orderName:string;
+  orderNumber:string;
+
+  orderNamePlaceHolder: string = "주문자명";
+  orderNumberPlaceHolder: string = "주문번호";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.loginTabsSelected = this.loginTabs[0];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MypagePage');
   }
 
+  loginTabChange(Category){
+    let idx = this.loginTabs.indexOf(Category);
+    this.loginTabsSelected = this.loginTabs[idx];
+  }
 }
