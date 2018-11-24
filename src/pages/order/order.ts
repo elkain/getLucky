99b5ded7549fec8c9975dfe93756aa5475721fdb
2Fe <hee861213@naver.com>
2018-11-24 09:04:37 +0900

    주문 페이지의 배송 정보 추가(ion-select 에서 option 부분 css 미완성)

--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -15,11 +15,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OrderPage {
 
+  deliveryCategorySelected;
+  basicPlaceStyle;
+  newPlaceStyle;
+  showDeliveryInfo:boolean=true;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.deliveryCategorySelected='basicPlace';
+    this.basicPlaceStyle = { 'select-segment': true, 'unselect-segment': false};
+    this.newPlaceStyle = { 'select-segment': false, 'unselect-segment': true };
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderPage');
   }
 
+  selectBasicPlaceSegment(){
+    this.basicPlaceStyle = { 'select-segment': true, 'unselect-segment': false };
+    this.newPlaceStyle = { 'select-segment': false, 'unselect-segment': true };
+    this.deliveryCategorySelected="basicPlace";
+    console.log(this.deliveryCategorySelected);
+    
+  }
+
+  selectNewPlaceSegment(){
+    this.basicPlaceStyle = { 'select-segment': false, 'unselect-segment': true };
+    this.newPlaceStyle = { 'select-segment': true, 'unselect-segment': false };
+    this.deliveryCategorySelected = "newPlace";
+    console.log(this.deliveryCategorySelected);
+  }
+
+  hideDeliveryInfo(){
+    console.log("button click");
+    
+    if(this.showDeliveryInfo==true){
+      this.showDeliveryInfo=false;
+    } else if (this.showDeliveryInfo == false){
+      this.showDeliveryInfo = true;
+    } else{
+      console.log("ShowDeliveryInfo error");
+    }
+  }
 }
